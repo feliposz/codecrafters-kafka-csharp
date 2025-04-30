85f181da21fb4bef4f04437ad2ef24b8551e19d8
feliposz <feliposz@gmail.com>
2025-04-30 02:47:28 -0300

    Parse Correlation ID

--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,4 @@
+using System.Buffers.Binary;
 using System.Net;
 using System.Net.Sockets;
 
@@ -5,9 +6,15 @@ TcpListener server = new TcpListener(IPAddress.Any, 9092);
 server.Start();
 Socket client = server.AcceptSocket();
 
-int messageSize = 0;
-int correlationId = 7;
+byte[] request = new byte[1024];
+int requestSize = client.Receive(request);
 
+int requestMessageSize = BinaryPrimitives.ReadInt32BigEndian(request.AsSpan()[0..4]);
+short requestApiKey = BinaryPrimitives.ReadInt16BigEndian(request.AsSpan()[4..6]);
+short requestApiVersion = BinaryPrimitives.ReadInt16BigEndian(request.AsSpan()[6..8]);
+int correlationId = BinaryPrimitives.ReadInt32BigEndian(request.AsSpan()[8..12]);
+
+int messageSize = 0;
 client.Send(BitConverter.GetBytes(messageSize).Reverse().ToArray());
 client.Send(BitConverter.GetBytes(correlationId).Reverse().ToArray());
 client.Close();
